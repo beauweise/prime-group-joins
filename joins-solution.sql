ebe4fa770a56f820866b6a7469790ff47ff066fc
Beau Weise <beauweise@gmail.com>
2020-11-04 08:53:15 -0600

    answered a few more

--- joins-solution.sql
+++ joins-solution.sql
@@ -7,13 +7,40 @@
 4	Lucy	Liu	5	3 Charles Dr	Los Angeles	CA	00000	home	4
 
 -- Get all orders and their line items (orders, quantity and product).
-
+1	2010-03-05	1	1	16	1	1
+1	2010-03-05	1	2	4	1	2
+1	2010-03-05	1	3	2	1	3
+2	2012-02-08	2	4	3	2	4
+2	2012-02-08	2	5	1	2	5
+3	2016-02-07	2	6	6	3	6
+4	2011-03-04	3	7	4	4	7
+4	2011-03-04	3	8	7	4	1
+4	2011-03-04	3	9	2	4	2
+4	2011-03-04	3	10	4	4	3
+4	2011-03-04	3	11	10	4	4
+4	2011-03-04	3	12	3	4	5
+5	2012-09-22	5	13	5	5	6
+5	2012-09-22	5	14	4	5	7
+5	2012-09-22	5	15	9	5	1
+5	2012-09-22	5	16	3	5	2
+5	2012-09-22	5	17	6	5	3
+6	2012-09-23	6	18	3	6	4
+6	2012-09-23	6	19	7	6	5
+6	2012-09-23	6	20	1	6	6
+6	2012-09-23	6	21	2	6	7
+6	2012-09-23	6	22	4	6	1
+6	2012-09-23	6	23	7	6	2
+7	2012-09-23	2	24	8	7	3
+7	2012-09-23	2	25	6	7	4
+7	2012-09-23	2	26	9	7	5
 
 -- Which warehouses have cheetos?
-
+Delta
 
 -- Which warehouses have diet pepsi?
-
+Alpha
+Delta
+Gamma
 
 -- Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
 
